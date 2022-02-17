09a940380f004d8d1cf8ea109060d48f37ceb9bb
sayeed <cs19btech11004@iith.ac.in>
2022-02-17 20:25:22 +0530

    cmm

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import socket
-sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)      # For UDP
-udp_host = socket.gethostname()		        # Host IP
-udp_port = 12345			                # specified port to connect
+sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+udp_host = socket.gethostname()	
+udp_port = 12345
 SIZE = 4096
 sock.bind((udp_host,udp_port))
 message = ''
