87326fe25e6e0fa46f7583e22264a4239bdcccac
sayeed <cs19btech11004@iith.ac.in>
2022-02-17 12:41:12 +0530

    file transfer completed

--- server.py
+++ server.py
@@ -1,10 +1,25 @@
+from ctypes.wintypes import SIZE
+from email import message
 import socket
 
 sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)      # For UDP
 udp_host = socket.gethostname()		        # Host IP
 udp_port = 12345			                # specified port to connect
+SIZE = 1024
 sock.bind((udp_host,udp_port))
+message = ''
 while True:
 	print ("Waiting for client...")
-	data = sock.recvfrom(1024)    
-	print ("Received Messages:",data[0].decode("utf-8"))
+	data = sock.recvfrom(SIZE)    
+	message = data[0].decode("utf-8")
+	if(message=='FILESENDING'):
+		file_name = sock.recvfrom(SIZE)
+		file_name  = file_name[0].decode("utf-8")
+		file = open(("received_folder/"+file_name),"wb")
+		data = sock.recvfrom(SIZE)
+		file.write(data[0])
+		while(len(data)>=SIZE):
+			data = sock.recvfrom(SIZE)
+			file.write(data[0])
+		file.close()
+		print("FILE Transferred Closed")
