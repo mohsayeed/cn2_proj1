2836a90dd5f21aedf562799b29b23c506d1a7516
sayeed <cs19btech11004@iith.ac.in>
2022-02-17 20:12:26 +0530

    udp ftp

--- client.py
+++ client.py
@@ -5,20 +5,21 @@ import socket
 sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 udp_host = socket.gethostname()
 udp_port = 12345
-SIZE = 1024
+SIZE = 4096
 msg = "Hello Python!"
 print ("UDP target IP:", udp_host)
 print ("UDP target Port:", udp_port)
 sock.sendto(bytes(msg,"utf-8"),(udp_host,udp_port))
-file_name = input("Please Enter the File Name")
-file = open("files_folder/"+file_name,"rb")
+file_name = input("Please Enter the File Name : ")
 sock.sendto(bytes("FILESENDING","utf-8"),(udp_host,udp_port))
 sock.sendto(bytes(file_name,"utf-8"),(udp_host,udp_port))
+file = open("files_folder/"+file_name,"rb")
 data = file.read(SIZE)
 while data:
     sock.sendto(data,(udp_host,udp_port))
     data = file.read(SIZE)
-    file.close()
+    print(data)
+file.close()
 print("data send from the client")
 
 
--- server.py
+++ server.py
@@ -5,7 +5,7 @@ import socket
 sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)      # For UDP
 udp_host = socket.gethostname()		        # Host IP
 udp_port = 12345			                # specified port to connect
-SIZE = 1024
+SIZE = 4096
 sock.bind((udp_host,udp_port))
 message = ''
 while True:
@@ -18,8 +18,8 @@ while True:
 		file = open(("received_folder/"+file_name),"wb")
 		data = sock.recvfrom(SIZE)
 		file.write(data[0])
-		while(len(data)>=SIZE):
+		while(len(data[0])>=SIZE):
 			data = sock.recvfrom(SIZE)
 			file.write(data[0])
 		file.close()
-		print("FILE Transferred Closed")
+		print("FILE Transferred Completed")
